de756dfe2de02d6616331d05b5f353ca5e769b4b
Direct-Dev-Ru <info@direct-dev.ru>
2021-06-09 18:04:43 +0600

    20210609-022 cors? bcryps added

--- api/app.js
+++ api/app.js
@@ -1,14 +1,21 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
-var sassMiddleware = require("node-sass-middleware");
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const sassMiddleware = require("node-sass-middleware");
+const cors = require("cors");
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
 
-var app = express();
+const app = express();
+
+const corsOptions = {
+  origin: "*"
+};
+
+app.use(cors(corsOptions));
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
