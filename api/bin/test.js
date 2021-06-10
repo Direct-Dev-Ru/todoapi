47df699da3471fbe2ddbfcbc3e43ca731b9b6067
Direct-Dev-Ru <info@direct-dev.ru>
2021-06-10 16:00:02 +0600

    20210610-002 bin\test.js added

---
+++ api/bin/test.js
@@ -0,0 +1,20 @@
+const db = require("../models");
+
+db.mongoose.Promise = global.Promise;
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+  autoIndex: false, // Don't build indexes
+  poolSize: 10, // Maintain up to 10 socket connections
+  // serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+  // socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+  family: 4 // Use IPv4, skip trying IPv6
+};
+db.mongoose.connect(db.config.mongoUri, options);
+db.connection = db.mongoose.connection
+  .then(() => {
+    console.log("connected to mongo db");
+  })
+  .catch((error) => console.log);
